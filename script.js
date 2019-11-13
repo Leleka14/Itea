a1cdb839e1cc521d9279da231cd3dd04211e8e75
Leleka14 <43849847+Leleka14@users.noreply.github.com>
2019-11-13 12:10:43 +0200

    updated in home

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-func = (a, b) => {
-	return a + b
+const user = {
+	name: 'Oleh',
+	age: 18
 }
 
-bar = (a, b) => {
-	[a,b] = [b,a]
-	console.log(a,b)
+const user2 = {
+	surname: 'Deneka'
 }
+
+user.__proto__ = user2
+
+
+let fruits = {
+  apple: 3,
+  lemon: 0,
+  bananas: 5,
+  pineapple: 0
+};
+
+let vegetables = {
+  potato: 2,
+  tomato: 1,
+  carrot: 0
+}
+Object.setPrototypeOf(fruits, vegetables)
+
+let sortedFruits = Object.create(null)
+for(let key in fruits){
+	if (fruits[key] > 0){
+		sortedFruits[key] = fruits[key]
+	}
+}
+
+console.log(sortedFruits)
