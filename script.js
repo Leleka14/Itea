82f1633b8388e3894e8bfe935f7ef520a61d4a33
Leleka14 <43849847+Leleka14@users.noreply.github.com>
2019-11-25 15:28:36 +0200

    updated before fourth lesson at home

--- script.js
+++ script.js
@@ -150,8 +150,6 @@
 
 // console.log(jillsRoom)
 
-// let btn = document.querySelector('button')
-
 // function createHandeler(element){
 // 	this.handler = function(){
 // 		element.addEventListener('click', (e) => {
@@ -175,4 +173,114 @@
 
 // console.log(greet('alex'));
 
+// (function(){
+// 	window.a = 21
+// })()  //anonym function
+// const a = 22
 
+// const singleTone = (function(){
+// 	let count = 0,
+// 		instanse
+// 	function increaseCount(){
+// 		count +=1
+// 	}
+// 	function showCount(){
+// 		return count
+// 	}
+// 	function createInstanse(){
+// 		return function(){
+// 			return{
+// 				increase:increaseCount,
+// 				show:showCount
+// 			}
+// 		}
+// 	}
+// 	return{
+// 		getInstance: function(){
+// 			if(!instanse){
+// 				instanse = createInstanse()
+// 			}
+// 			return instanse
+// 		}
+// 	}
+// })()
+
+// const pull = singleTone.getInstance()()
+
+// let makeRed = document.querySelector('.red')
+// let makeBlue = document.querySelector('.blue')
+// let makeOrange = document.querySelector('.orange')
+
+// let btn = document.querySelector('button')
+// let count = 0
+// let div1 = document.getElementById('text')
+
+clickFunction = (element) =>{
+	element.addEventListener('click', () =>{
+		console.log(`User clicked ${count} times`)
+		div1.innerHTML = `User clicked ${count} times`
+		count++;
+	})
+}
+
+// clickFunction(btn);
+
+
+colorFunction = (id) => {
+	const array = document.getElementById(id).childNodes;
+	console.log(array)
+	for(let i in array){
+		if(array[i].nodeName === 'LI'){
+			if(i < 9){
+				array[i].style.backgroundColor = 'red'
+			}
+			else if(i >= 9 && i < 15){
+				array[i].style.backgroundColor = 'blue'
+			}
+			else{
+				array[i].style.backgroundColor = 'orange'
+			}
+		}
+	}
+
+	// array[0].parentNode.lastChild.previousSibling.style.backgroundColor = 'orange'
+}
+
+// colorFunction('list')
+
+
+// loopForSetColors = (list, color) => {
+// 	for(let i = 0; i < list.length; i++){
+// 		list[i].style.backgroundColor = color
+// 	}
+// }
+
+// colorFunction1 = () => {
+// 	const makeRed = document.getElementsByClassName('red')
+// 	const makeBlue = document.getElementsByClassName('blue')
+// 	const makeOrange = document.getElementsByClassName('orange')
+// 	loopForSetColors(makeRed, 'red')
+// 	loopForSetColors(makeBlue, 'blue')
+// 	loopForSetColors(makeOrange, 'orange')
+	
+// }
+
+// colorFunction1()
+
+// const butt = document.getElementById('body')
+
+// settingBordersFunction = (element) => {
+// 	element.addEventListener('click', () =>{
+// 		document.body.style.border = 'thick solid #0000FF'
+// 	})
+// }
+
+// settingBordersFunction(butt)
+// const bodyDiv = document.querySelector('body')
+// const createdH = document.createElement('h1')
+// createdH.textContent = bodyDiv.innerHTML
+// bodyDiv.appendChild(createdH
+
+for(let i = 0; i < document.body.childNodes.length; i++){
+	console.log(document.body.childNodes[i].data)
+}
---
+++ script2.js
@@ -0,0 +1,1 @@
+// const pull2 = pull
