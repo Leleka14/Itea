368bb79eb035757d3442e571e1796ff9e76820cc
Leleka14 <43849847+Leleka14@users.noreply.github.com>
2019-12-02 01:26:20 +0200

    upd at 02.12.19

--- TicTacToe/ticTacToeScript.js
+++ TicTacToe/ticTacToeScript.js
@@ -1,49 +1,69 @@
 const app = {}
 app.elements = {}
 app.methods = {}
-app.elements.allBoxes = document.querySelector('.box')
+app.elements.players = {};
 app.elements.boxContainer = document.querySelector('#blocks')
 app.elements.turnToGo = document.querySelector('#turnToGo')
+app.elements.players.player1ScoreSpan = document.querySelector('#user1Score')
+app.elements.players.player2ScoreSpan = document.querySelector('#user2Score')
+app.elements.players.firstPlayerImage = `cross`;
+app.elements.players.secondPlayerImage = `circle`;
+app.elements.players.firstPlayerTurnToGo = `It's Player's 1 turn to go...`;
+app.elements.players.secondPlayerTurnToGo = `Player 2, your move!`
+app.elements.players.firstPlayerValue = 1;
+app.elements.players.secondPlayerValue = 2;
 app.elements.currentBox = null;
-app.elements.player = {};
-app.elements.player.first = `cross`;
-app.elements.player.second = `circle`;
 app.elements.image = null
+for(let i = 0; i < app.elements.boxContainer.children.length; i++){
+	app.elements.boxContainer.children[i].value = 0;
+}
+
 
 firstGoes = () =>{
-	const randomNumber = (Math.floor(Math.random() * 2))
+	const randomNumber = (Math.floor(Math.random() * 2));
 	return randomNumber;
 }
 
-const rand = firstGoes()
+const rand = firstGoes();
 
 if(rand <= 0.5){
-	app.elements.turnToGo.textContent = `Player 1 goes first`;
-	for(let i = 0; i < app.elements.boxContainer.children.length; i++){
-		if(i % 2 != 0){
-			app.elements.image = 'cross'
-		}
-		else{
-			app.elements.image = 'circle'
-		}
-	}
+	app.elements.turnToGo.textContent = app.elements.players.firstPlayerTurnToGo;
 }
 else{
-	app.elements.turnToGo.textContent = `Player 2 goes first`;
+	app.elements.turnToGo.textContent = app.elements.players.secondPlayerTurnToGo;
 }
 
 setImageFunction = (element) => {
+	if(app.elements.turnToGo.textContent === app.elements.players.firstPlayerTurnToGo){
+		app.elements.image = app.elements.players.firstPlayerImage;
+		app.elements.currentBox.value = app.elements.players.firstPlayerValue;
+		app.elements.turnToGo.textContent = app.elements.players.secondPlayerTurnToGo;
+	}
+	else{
+		app.elements.image = app.elements.players.secondPlayerImage;
+		app.elements.currentBox.value = app.elements.players.secondPlayerValue;
+		app.elements.turnToGo.textContent = app.elements.players.firstPlayerTurnToGo;
+	}
 	element.style.cssText = `
 		background-image: url('Images/${app.elements.image}.png');
 		background-repeat: no-repeat;
 		background-size: 100%;`
 }
 
-app.methods.setCross = (function() {
+app.methods.setBackgroundInClick = (function() {
 	app.elements.boxContainer.addEventListener('click', (e) =>{
-		currentBox = e.target;
-		if(currentBox.tagName.toLowerCase() === 'div'){
-			setImageFunction(currentBox)
+		app.elements.currentBox = e.target;
+		if(app.elements.currentBox.tagName.toLowerCase() === 'div' && app.elements.currentBox.value === 0){
+			setImageFunction(app.elements.currentBox)
 		}
 	})
 })()
+
+// app.methods.check = (function() {
+// 	for(let i = 0; i < app.elements.allBoxes.length; i++){
+// 		if()
+// 	}
+// })()
+
+
+
